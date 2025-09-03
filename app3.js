cb3d768f40bfe9eb44eb61ca9a24ff39222fccb4
antonius-ras <antonius.19115@gmail.com>
2025-09-03 17:20:32 +0700

    missing 2

--- app3.js
+++ app3.js
@@ -1,9 +1,55 @@
-let students ={
-    name: "tobias",
-    grade: "A"
-};
+let students = [
+    {
+        id: "66111111",
+        name: "สมชาย",
+        major: "วิศวกรรมคอมพิวเตอร์",
+        grade: "3.45"
+    },
+    {
+        id: "66222222",
+        name: "สมหญิง",
+        major: "วิทยาการคอมพิวเตอร์",
+        grade: "3.02"
+    },
+    {
+        id: "66333333",
+        name: "สมศรี",
+        major: "เทคโนโลยีสารสนเทศ",
+        grade: "2.98"
+    },
+    {
+        id: "664444444",
+        name: "สมปอง",
+        major: "วิศวกรรมการบินและอวกาศ",
+        grade: "4.00"
+    },
+    {
+        id: "66555555",
+        name: "สมฤดี",
+        major: "วิทยาศาสตร์ชีวการแพทย์",
+        grade: "3.25"
+    },
+    {
+        id: "66666666",
+        name: "สมบัติ",
+        major: "วิศวกรรมเคมีและวัสดุ",
+        grade: "1.88"
+    },
+    {
+        id: "66777777",
+        name: "สมฤทธิ์",
+        major: "วิศวกรรมคอมพิวเตอร์",
+        grade: "3.98"
+    }
+];
 
+students.forEach((student, index) => {
+    console.log(`นักศึกษา : ${index}, Name : ${student.name}, GPAX : ${student.grade}`);
+});
 
-function showStudentsCount(){
-
+function showStudentCount() {
+    let studentCount = students.length;
+    console.log(`นักศึกษาทั้งหมด : ${studentCount}`);
 }
+
+showStudentCount();
